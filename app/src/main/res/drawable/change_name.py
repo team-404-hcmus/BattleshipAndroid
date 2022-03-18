47614306d0391ca19fc9fba61bfa99e7264c713f
Nguyễn Anh Tuấn <19127614@student.hcmus.edu.vn>
2022-03-19 00:50:57 +0700

    fix conflict file

--- app/src/main/res/drawable/change_name.py
+++
@@ -1,8 +0,0 @@
-import os
-import fnmatch
-
-dir = os.listdir()
-filtered = fnmatch.filter(dir,"paper_*.jpg")
-for idx,value in enumerate(filtered):
-    print("renaming: ",value)
-    os.rename(value,"paper_"+str(idx)+".jpg")
